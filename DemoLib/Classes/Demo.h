365d15454f5892e79cf41ed1b5399853ddc75566
MrVking <523439877@qq.com>
2017-07-23 22:54:45 +0800

    versio-1

---
+++ DemoLib/Classes/Demo.h
@@ -0,0 +1,15 @@
+//
+//  Demo.h
+//  DemoLib
+//
+//  Created by apple on 2017/7/23.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Demo : NSObject
+
++ (void)log;
+
+@end
---
+++ DemoLib/Classes/Demo.m
@@ -0,0 +1,22 @@
+//
+//  Demo.m
+//  DemoLib
+//
+//  Created by apple on 2017/7/23.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import "Demo.h"
+
+@implementation Demo
+
++ (void)log
+{
+    NSLog(@"1");
+    
+    NSLog(@"2");
+    
+    NSLog(@"3");
+}
+
+@end
